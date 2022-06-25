8aa6bc1aba2e85c14636745cd4392f875841155e
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2022-06-25 09:29:35 +0200

    Update GeoROC_viewer.py

--- GeoROC_viewer.py
+++ GeoROC_viewer.py
@@ -20,10 +20,5 @@ st.header('Welcome to the Home of Geoscience Apps')
 
 st.subheader('Choose where to go next:')
 
-col1, col2, col3 = st.columns(3)
 
-with col1:
-    st.markdown("[![Foo](https://raw.githubusercontent.com/Hezel2000/GeoDataScience/main/icons/flank%20method%20small.jpg)](https://hezel2000-flank-method-flank-data-reduction-2ncvvv.streamlitapp.com)")
-    
-with col2:
-    st.markdown("[![Foo](https://raw.githubusercontent.com/Hezel2000/GeoDataScience/main/icons/MetBase%20Logo.jpg)](https://metbase.org)")
+st.sidebar.multiselect('sel',[1,2,3])
