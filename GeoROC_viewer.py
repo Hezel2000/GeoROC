f1f89b2da0386f32f31c7fe343e181c3451d1590
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2022-06-25 14:17:45 +0200

    Update GeoROC_viewer.py

--- GeoROC_viewer.py
+++ GeoROC_viewer.py
@@ -19,26 +19,42 @@ import os
 import pandas as pd
 import numpy as np
 import pydeck as pdk
+from bokeh.plotting import figure
+from bokeh.models import Legend
 from random import randrange
 
 st.header('Welcome to GeoROC Viewer')
 
 tectSettingsPath = '/Users/dominik/Documents/GitHub/GeoROC/data/'
+el = pd.read_csv(tectSettingsPath + 'Archean Cratons/Bastar Craton.csv').columns[27:100]
 
-tectSettings = os.listdir(tectSettingsPath)
-st.write(tectSettings)
+tectSettingsFolder = os.listdir(tectSettingsPath)
 
-tectSettingsFolder = st.sidebar.multiselect('sel', tectSettings[1:],tectSettings[1])
+tectSettings=[]
+for i in tectSettingsFolder:
+    if os.path.isdir(tectSettingsPath + i):
+        tectSettings.append(i)
+  
+
+tectSettingsFolder = st.sidebar.selectbox('sel', tectSettings)
+
+tectSettingsContent = os.listdir(tectSettingsPath + tectSettingsFolder)
 
 
-tectSettingsContent = os.listdir(tectSettingsPath + tectSettingsFolder[0])
+xAxisScatterData = st.sidebar.selectbox('x-axis', el)
+yAxisScatterData = st.sidebar.selectbox('y-axis', el)
+
+fig = figure(width=600, height=400)
+
 selLatLonList=[]
+legend_it = []
 for file in tectSettingsContent:
     if file.endswith('.csv'):
-        singleTectSetting = tectSettingsPath + tectSettingsFolder[0] + '/' + file
+        singleTectSetting = tectSettingsPath + tectSettingsFolder + '/' + file
     readTectSetting = pd.read_csv(singleTectSetting)
     selLatLon = readTectSetting[['Latitude (Min)', 'Longitude (Min)']]
     selLatLon = selLatLon.rename(columns={'Latitude (Min)':'lat', 'Longitude (Min)':'lon'})
+    randColor = [randrange(255), randrange(255), randrange(255)]
     layers = pdk.Layer(
         'ScatterplotLayer',
            selLatLon,
@@ -52,20 +68,28 @@ for file in tectSettingsContent:
            line_width_min_pixels=1,
            get_position='[lon, lat]',
            get_radius="exits_radius",
-           get_fill_color=[randrange(255), randrange(255), randrange(255)],
+           get_fill_color=randColor,
            get_line_color=[0, 0, 0],
     )
     selLatLonList.append(layers)
-
-
+    c = fig.circle(readTectSetting[xAxisScatterData]/10000, readTectSetting[yAxisScatterData]/10000, color=tuple(randColor))
+    fig.xaxis.axis_label = xAxisScatterData + ' wt%'
+    fig.yaxis.axis_label = yAxisScatterData + ' wt%'
+    legend_it.append((file, [c]))
+    
 st.pydeck_chart(pdk.Deck(
      map_style='mapbox://styles/mapbox/light-v9',
      initial_view_state=pdk.ViewState(
-         latitude=37.76,
-         longitude=-122.4,
-         zoom=11,
+         latitude=50.110924,
+         longitude=8.682127,
+         zoom=2,
          height=500,
          width=800
      ),
      layers=selLatLonList,
  ))
+
+legend = Legend(items=legend_it)
+fig.add_layout(legend,'left')
+
+st.bokeh_chart(fig)
